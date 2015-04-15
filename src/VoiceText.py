eadcf67f402b4f69a6c6eee52389fe7240ac8a64
Cheng LUO <luochengleo@gmail.com>
2015-04-15 17:01:10 +0800

    sa

--- src/VoiceText.py
+++ src/VoiceText.py
@@ -1,21 +1,29 @@
 __author__ = 'cheng'
 import json
 fout = open('../data/voice2text.csv','w')
-for l in open('../data/voice2text.txt').readlines():
-    count = 0
-    user,filename,jcontent = l.strip().split('\t')
-    if '_' in filename:
-        if jcontent=='':
-            print 'empty'
-            fout.write('"'+user+'","'+filename+'"\n')
+count = 0
+wcount = 0
+for l in open('../data/result.txt').readlines():
+    count +=1
+    segs = l.strip().split('\t')
+    if len(segs) >=3:
+        user,filename,jcontent = l.strip().split('\t')
+    else:
+        user,filename = l.strip().split('\t')
+        jcontent=''
+    filename = filename.replace('.wav','').replace('_','","')
+    if jcontent=='':
+        print 'empty'
+        fout.write('"'+user+'","'+filename+'"\n')
+        continue
+    else:
+        jobj = json.loads(jcontent)
+        if 'result' in jobj.keys():
+            wcount +=1
+            print wcount,count
+            fout.write('"'+user+'","'+filename+'","'+jobj['result'][0].encode('cp936')+'"\n')
             continue
         else:
-            jobj = json.loads(jcontent)
-            if 'result' in jobj.keys():
-                print 1
-                fout.write('"'+user+'","'+filename+'","'+jobj['result'][0].encode('cp936')+'"\n')
-                continue
-            else:
-                print 2
-                fout.write('"'+user+'","'+filename+'"\n')
-                continue
+            print 2
+            fout.write('"'+user+'","'+filename+'"\n')
+            continue
